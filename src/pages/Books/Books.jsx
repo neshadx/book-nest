c7a4c34ba1de1a5f31b01205f6654810c528a009
neshadx <neshadimagify@gmail.com>
2025-04-19 21:07:36 +0600

    added some code

---
+++ src/pages/Books/Books.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Books = () => {
+    const [allBooks,setAllBooks] = useState([]);
+
+    useEffect(()=>{
+        fetch("booksData.json")
+        .then(res=> res.json())
+        .then(data =>{
+            setAllBooks(data)
+        })
+    },[])
+
+    return (
+        <div>
+            <h1>Hey Books Here</h1>
+        </div>
+    );
+};
+
+export default Books;
