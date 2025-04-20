e3317adbe13f5dfecc9e2abaa94529663f1959a9
neshadx <neshadimagify@gmail.com>
2025-04-20 19:54:40 +0600

    added some code

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -28,6 +28,7 @@ export const router = createBrowserRouter([
         },
         {
           path: '/bookDetails/:id',
+          loader: ()=>fetch('./booksData.json'),
           Component:BookDetails
         }
       ]
--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+
+   const {id} = useParams ();
+   const bookId = parseInt(id);
+   const data = useLoaderData ();
+   const singleBook = data.find(book => book.bookId === bookId);
+   const {bookName, image} = singleBook;
+
+
+
+ const handleMarkAsRead   
     return (
-        <div>
-            <h1>Book Details</h1>
+        <div className=' w-2/3 mx-auto'>
+            <img className='w-48' src={image} alt="" />
+            <h5>{bookName}</h5>
+            <button className="btn btn-accent m-2">Read</button>
+            <button className="btn btn-info m-2">WishList</button>
+            
         </div>
     );
 };
