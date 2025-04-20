81805b38f01803a8352b3d2c12ccca51cdc2ba18
neshadx <neshadimagify@gmail.com>
2025-04-20 22:27:01 +0600

    added some code

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -7,20 +7,48 @@ import Book from "../Book/Book";
 
 
 const ReadList = () => {
+  const [readList, setReadList] = useState([]);
+  const [sort,setSort] = useState("");
+  const data = useLoaderData();
 
-const [readList,setReadList] = useState([]) 
- const data = useLoaderData();
-
- useEffect(() => {
+  useEffect(() => {
     const storedBookData = getStoredBook();
-    const ConvertedStoredBooks = storedBookData.map(id => parseInt(id))
-    const myReadList = data.filter(book=>ConvertedStoredBooks.includes(book.bookId));
-    setReadList(myReadList)
- },[])  
+    const ConvertedStoredBooks = storedBookData.map((id) => parseInt(id));
+    const myReadList = data.filter((book) =>
+      ConvertedStoredBooks.includes(book.bookId)
+    );
+    setReadList(myReadList);
+  }, []);
+
+const handleSort=(type) => {
+    setSort(type)
+    if(type === "pages") {
+        const sortedByPage=[...readList].sort((a,b) => a.totalPages - b.totalPages);
+        setReadList(sortedByPage)
+
+    } 
+        if (type === "ratings") {
+           const sortedByRating = [...readList].sort((a,b)=>a.rating - b.rating)
+           setReadList(sortedByRating)
+        }
+}
+
+
 
 
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by : {sort?sort:""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={()=>handleSort("pages")}>Pages</a>
+          </li>
+          <li>
+            <a onClick={()=>handleSort("ratings")}>Ratings</a>
+          </li>
+        </ul>
+      </details>
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
@@ -30,9 +58,9 @@ const [readList,setReadList] = useState([])
         <TabPanel>
           <h2>Book Read Already {readList.length}</h2>
 
-          {
-            readList.map(b=><Book key={b.bookId} singleBook={b}></Book>)
-          }
+          {readList.map((b) => (
+            <Book key={b.bookId} singleBook={b}></Book>
+          ))}
         </TabPanel>
         <TabPanel>
           <h2>Wish List Done</h2>
