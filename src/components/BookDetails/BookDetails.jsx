f0093f686ac569671f4207cf4aa1a50080e31923
neshadx <neshadimagify@gmail.com>
2025-04-20 22:46:46 +0600

    added some code

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addStoredDB } from '../../utility/addToDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+import { ToastContainer, toast } from 'react-toastify';
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
 
@@ -14,6 +20,13 @@ const BookDetails = () => {
 
  const handleMarkAsRead = id => {
 
+    // MySwal.fire({
+    //     title: "Good job!",
+    //     text: "You clicked the button!",
+    //     icon: "success"
+    //   });
+
+    toast("Book Marked done!")
     addStoredDB(id)
 
  }  
@@ -21,6 +34,7 @@ const BookDetails = () => {
         <div className=' w-2/3 mx-auto'>
             <img className='w-48' src={image} alt="" />
             <h5>{bookName}</h5>
+            <ToastContainer />
             <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Read</button>
             <button className="btn btn-info m-2">WishList</button>
             
